89cf37c2fa3106bdc3e482838b045a3ba6fbba95
hasini <hasini.psk@gmail.com>
2011-09-12 02:18:10 +0530

    login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,3 +47,10 @@ Calculator::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
+
+
+GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
+   :login => "seller_1229899173_biz_api1.railscasts.com",
+   :password => "FXWU58S7KXFC6HBE",
+   :signature => "AGjv6SW.mTiKxtkm6L9DcSUCUgePAUDQ3L-kTdszkPG8mRfjaRZDYtSu"
+ )
