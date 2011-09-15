5283bdd74b85048741720007fd338b6b5ef0c64e
hasini <hasini.psk@gmail.com>
2011-09-16 01:13:33 +0530

    layouts chnege

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,8 @@ class OrdersController < ApplicationController
 
   skip_before_filter :check_user_status, :only => [:new,:create,:sucess,:failure,:calculate_amount,:coupons_form]
 #  skip_before_filter :check_for_access_period
+
+  
   def index
     @orders = Order.all
 
